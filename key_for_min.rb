5c0e328dcb0758bba8a48ff97aebd46c9f2e23b1
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-24 19:13:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,23 +3,21 @@
 
 def key_for_min_value(hash)
 
+    min_age = 10000
+  min_name = nil
+
 if hash.length == 0 
   return nil
   
+  
 else 
   
-  min_age = 10000
-  min_name = nil
+
   
   hash.each do |name, age|
     if age < min_age
       min_age = age
       min_name = name
-      
-    else
-      
-            min_age = min_age
-      min_name = min_name
       
     end
     
